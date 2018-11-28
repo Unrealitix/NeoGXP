5e11259499555c1b0e45b9771c2de167b85732c5
Paul Bonsma <p.s.bonsma@saxion.nl>
2018-11-28 18:30:13 +0100

    Removing LateAdd from HierarchyManager. With the current setup (GameObjects are added to the Update & OnCollision loops as soon as they are created), this makes no sense.

--- GXPEngine/GXPEngine/AddOns/HierarchyManager.cs
+++ GXPEngine/GXPEngine/AddOns/HierarchyManager.cs
@@ -2,19 +2,10 @@ using GXPEngine;
 using System.Collections.Generic;
 
 namespace GXPEngine {
-	struct GameObjectPair {
-		public GameObject parent;
-		public GameObject child;
-		public GameObjectPair(GameObject par, GameObject ch) {
-			parent = par;
-			child = ch;
-		}
-	}
-
 	/// <summary>
-	/// If you are getting strange bugs because you are calling AddChild or Destroy during the Update loop, 
+	/// If you are getting strange bugs because you are calling Destroy during the Update loop, 
 	/// you can use this class to do this more cleanly: when using 
-	/// HierarchyManager.Instance.LateAdd or HierarchyManager.Instance.LateDestroy,
+	/// HierarchyManager.Instance.LateDestroy,
 	/// all these hierarchy changes will be made after the update loop is finished.
 	/// Similarly, you can use HierarchyManager.Instance.LateCall to postpone a certain method call until 
 	/// after the update loop.
@@ -33,23 +24,14 @@ namespace GXPEngine {
 		private static HierarchyManager instance; 
 
 		private List<GameObject> toDestroy;
-		private List<GameObjectPair> toAdd;
 		private List<DelayedMethod> toCall;
 
 		// Don't construct these yourself - get the one HierarchyManager using HierarchyManager.Instance
 		HierarchyManager() {
 			Game.main.OnAfterStep += UpdateHierarchy;
 			toDestroy = new List<GameObject> ();
-			toAdd = new List<GameObjectPair> ();
 			toCall = new List<DelayedMethod> ();
 		}
-
-		/*
-		public void Destroy() {
-			Game.main.OnAfterStep -= UpdateHierarchy;
-			instance = null;
-		}
-		*/
 
 		public void LateDestroy(GameObject obj) {
 			toDestroy.Add (obj);
@@ -59,21 +41,11 @@ namespace GXPEngine {
 			return toDestroy.Contains (obj);
 		}
 
-		public void LateAdd(GameObject parent, GameObject child) {
-			toAdd.Add (new GameObjectPair (parent, child));
-		}
-
 		public void LateCall(DelayedMethod meth) {
 			toCall.Add (meth);
 		}
 
 		public void UpdateHierarchy() {
-			// First add, then destroy, to prevent memory leaks (e.g. adding a projectile in a level that's about to be destroyed!)
-			foreach (GameObjectPair pair in toAdd) {
-				pair.parent.AddChild (pair.child);
-			}
-			toAdd.Clear ();
-
 			foreach (GameObject obj in toDestroy) {
 				obj.Destroy ();
 			}
