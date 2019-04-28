fc54338a5454e55ab798d8e382eaf83b83d6c76f
Paul Bonsma <p.s.bonsma@saxion.nl>
2019-04-28 21:45:36 +0200

    Fixing both hittests in BoxCollider for the case of skewed sprites (which can happen when combining non-uniform scaling for a parent with child rotations), using true SAT.

--- GXPEngine/GXPEngine/Core/BoxCollider.cs
+++ GXPEngine/GXPEngine/Core/BoxCollider.cs
@@ -43,50 +43,56 @@ namespace GXPEngine.Core
 		//														areaOverlap()
 		//------------------------------------------------------------------------------------------------------------------------
 		private bool areaOverlap(Vector2[] c, Vector2[] d) {
-			
-			float dx = c[1].x - c[0].x;
-			float dy = c[1].y - c[0].y;
-			float lengthSQ = (dy * dy + dx * dx);
-			
-			if (lengthSQ == 0.0f) lengthSQ = 1.0f;
-			
+			// normal 1:
+			float ny = c[1].x - c[0].x;
+			float nx = c[0].y - c[1].y;
+			// own 'depth' in direction of this normal:
+			float dx = c[3].x - c[0].x;
+			float dy = c[3].y - c[0].y;
+			float dot = (dy * ny + dx * nx);
+
+			if (dot == 0.0f) dot = 1.0f;
+
 			float t, minT, maxT;
-			
-			t = ((d[0].x - c[0].x) * dx + (d[0].y - c[0].y) * dy) / lengthSQ;
+
+			t = ((d[0].x - c[0].x) * nx + (d[0].y - c[0].y) * ny) / dot;
 			maxT = t; minT = t;
-			
-			t = ((d[1].x - c[0].x) * dx + (d[1].y - c[0].y) * dy) / lengthSQ;
+
+			t = ((d[1].x - c[0].x) * nx + (d[1].y - c[0].y) * ny) / dot;
 			minT = Math.Min(minT,t); maxT = Math.Max(maxT,t);
-			
-			t = ((d[2].x - c[0].x) * dx + (d[2].y - c[0].y) * dy) / lengthSQ;
+
+			t = ((d[2].x - c[0].x) * nx + (d[2].y - c[0].y) * ny) / dot;
 			minT = Math.Min(minT,t); maxT = Math.Max(maxT,t);
-			
-			t = ((d[3].x - c[0].x) * dx + (d[3].y - c[0].y) * dy) / lengthSQ;
+
+			t = ((d[3].x - c[0].x) * nx + (d[3].y - c[0].y) * ny) / dot;
 			minT = Math.Min(minT,t); maxT = Math.Max(maxT,t);
-			
-			if ((minT >= 1) || (maxT < 0)) return false;
-			
-			dx = c[3].x - c[0].x;
-			dy = c[3].y - c[0].y;
-			lengthSQ = (dy*dy + dx*dx);
-			
-			if (lengthSQ == 0.0f) lengthSQ = 1.0f;
-			
-			t = ((d[0].x - c[0].x) * dx + (d[0].y - c[0].y) * dy) / lengthSQ;
+
+			if ((minT >= 1) || (maxT <= 0)) return false;
+
+			// second normal:
+			ny = dx;
+			nx = -dy;
+			dx = c[1].x - c[0].x;
+			dy = c[1].y - c[0].y;
+			dot = (dy * ny + dx * nx);
+
+			if (dot == 0.0f) dot = 1.0f;
+
+			t = ((d[0].x - c[0].x) * nx + (d[0].y - c[0].y) * ny) / dot;
 			maxT = t; minT = t;
-			
-			t = ((d[1].x - c[0].x) * dx + (d[1].y - c[0].y) * dy) / lengthSQ;
+
+			t = ((d[1].x - c[0].x) * nx + (d[1].y - c[0].y) * ny) / dot;
 			minT = Math.Min(minT,t); maxT = Math.Max(maxT,t);
-			
-			t = ((d[2].x - c[0].x) * dx + (d[2].y - c[0].y) * dy) / lengthSQ;
+
+			t = ((d[2].x - c[0].x) * nx + (d[2].y - c[0].y) * ny) / dot;
 			minT = Math.Min(minT,t); maxT = Math.Max(maxT,t);
-			
-			t = ((d[3].x - c[0].x) * dx + (d[3].y - c[0].y) * dy) / lengthSQ;
+
+			t = ((d[3].x - c[0].x) * nx + (d[3].y - c[0].y) * ny) / dot;
 			minT = Math.Min(minT,t); maxT = Math.Max(maxT,t);
-			
-			if ((minT >= 1) || (maxT < 0)) return false;
-			
-			return true;			
+
+			if ((minT >= 1) || (maxT <= 0)) return false;
+
+			return true;
 		}
 		
 		//------------------------------------------------------------------------------------------------------------------------
@@ -94,25 +100,25 @@ namespace GXPEngine.Core
 		//------------------------------------------------------------------------------------------------------------------------
 		//ie. for hittestpoint and mousedown/up/out/over
 		private bool pointOverlapsArea(Vector2 p, Vector2[] c) {
-			
-			float dx = c[1].x - c[0].x;
-			float dy = c[1].y - c[0].y;
-			float lengthSQ = (dy * dy + dx * dx);
-			
+			float dx1 = c[1].x - c[0].x;
+			float dy1 = c[1].y - c[0].y;
+			float dx2 = c[3].x - c[0].x;
+			float dy2 = c[3].y - c[0].y;
+			// first: take delta1 as normal:
+			float dot = dy2 * dx1 - dx2 * dy1; 
+
 			float t;
-			
-			t = ((p.x - c[0].x) * dx + (p.y - c[0].y) * dy) / lengthSQ;
-			
+
+			t = ((p.y - c[0].y) * dx1 - (p.x - c[0].x) * dy1) / dot;
+			if ((t > 1) || (t < 0))	return false;
+
+			// next: take delta2 as normal:
+			dot = -dot;
+
+			t = ((p.y - c[0].y) * dx2 - (p.x - c[0].x) * dy2) / dot;
+
 			if ((t > 1) || (t < 0)) return false;
-			
-			dx = c[3].x - c[0].x;
-			dy = c[3].y - c[0].y;
-			lengthSQ = (dy*dy + dx*dx);
-			
-			t = ((p.x - c[0].x) * dx + (p.y - c[0].y) * dy) / lengthSQ;
-			
-			if ((t > 1) || (t < 0)) return false;
-			
+
 			return true;			
 		}	
 	}
